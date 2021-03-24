539d4959cbb091cccfcd23830d979cc564112db9
Hubert MUGABO <mugberto@gmail.com>
2021-03-25 01:15:14 +0200

    Add unique index for the username column

---
+++ db/migrate/20210324224534_add_unique_idx_to_users.rb
@@ -0,0 +1,5 @@
+class AddUniqueIdxToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_index :users, :username, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_154436) do
+ActiveRecord::Schema.define(version: 2021_03_24_224534) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
