35e84056564e4eed5587203b50f420ec619073cf
Hubert MUGABO <mugberto@gmail.com>
2021-03-25 00:35:53 +0200

    Add validation to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create; end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to :new
+    else
+      render :new
+    end
+  end
 
   def show; end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
 end
