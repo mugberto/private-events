853000e8d3b1e708d620024dd3ae811b10eb88fe
Hubert MUGABO <mugberto@gmail.com>
2021-03-27 00:37:19 +0200

    Add ::past and ::upcomming class method to event model
    Separate events on the index page into past and upcomming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :user, class_name: 'User'
   has_many :attendances
   has_many :attendees, through: :attendances, source: :attendee
+
+  def self.past
+    where('date < ?', DateTime.now)
+  end
+
+  def self.upcoming
+    where('date > ?', DateTime.now)
+  end
 end
