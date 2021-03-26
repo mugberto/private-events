9d5bc83e7c34550f5d69e30bf7b976f7151b50ce
Hubert MUGABO <mugberto@gmail.com>
2021-03-27 01:38:19 +0200

    Change root page to events#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'user_sessions/new', to: 'user_sessions#new', as: 'log_in'
   get 'user_sessions/create', to: 'user_sessions#create'
   get 'user_sessions/destroy', to: 'user_sessions#destroy'
-  root to: 'user_sessions#new'
+  root to: 'events#index'
   resources :users, only: [:new, :create, :show]
   resources :user_sessions, only: [:new, :create, :destroy]
   resources :events, only: [:index, :show, :new, :create]
