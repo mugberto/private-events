7d0bff4eed3db468fd4b99d96e031da8ec90a727
Hubert MUGABO <mugberto@gmail.com>
2021-03-28 11:17:25 +0200

    Add uniqueness tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'Validations' do
-    it 'should be valid if username is present' do
-      user = User.new(username: 'anyusername')
-      expect(user).to be_valid
+    it 'should be valid if username is present(presence)' do
+      expect(User.new(username: 'anyusername')).to be_valid
     end
-    it 'should be invalid if username is not present' do
-      user = User.new(username: nil)
-      expect(user).to be_invalid
+    it 'should be invalid if username is not present(presence)' do
+      expect(User.new(username: nil)).to be_invalid
+    end
+    it 'should be invalid if a username created already exists(uniqueness)' do
+      User.create(username: 'this_username')
+      expect(User.new(username: 'this_username')).to be_invalid
+    end
+    it 'should be valid if a username created is not taken(uniqueness)' do
+      expect(User.new(username: 'this_username')).to be_valid
     end
   end
 end
